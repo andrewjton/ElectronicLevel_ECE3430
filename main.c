835e0b7772c28796e81f1143ff66337cc22ede50
andrewton <andrewton@verizon.net>
2017-04-12 14:42:50 -0400

    added timerA files to configure brightness for LED's

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 /*
  * main.c
  */
+
+unsigned int period = 999;
+unsigned int direction = 0;
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+    InitializeLEDPortPins();
+
 	return 0;
 }
