614046dd1bf319c3e3f2c82fc216a9190577fe51
Mansoor Syed <syedm101@gmail.com>
2017-04-21 17:22:05 -0400

    calibration code working

---
+++ orientation_calculation.h
@@ -0,0 +1,17 @@
+/*
+ * orientation_calculation.h
+ *
+ *  Created on: Apr 21, 2017
+ *      Author: syedm_000
+ */
+
+#ifndef ORIENTATION_CALCULATION_H_
+#define ORIENTATION_CALCULATION_H_
+
+#include "LEDDisplay.h"
+
+LEDDirection getDirection();
+
+
+
+#endif /* ORIENTATION_CALCULATION_H_ */
